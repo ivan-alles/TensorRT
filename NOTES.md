122a8675fdd314cd9ca00749f54895936cb3e4a0
Ivan Alles <ivan.alles@gmail.com>
2022-03-31 16:27:22 +0200

    Use env variable CudaToolkitDir.

--- NOTES.md
+++ NOTES.md
@@ -1,1 +1,3 @@
 Windows 10 SDK (10.0.17134.0) was installed to build TensorRT sample_mnist.sln 
+
+To locate CUDA Toolkit for Visual Studio, set environment variable CudaToolkitDir to C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4 
--- build.ps1
+++ build.ps1
@@ -1,17 +1,36 @@
 $Env:TRT_OSSPATH = "D:\ivan\projects\TensorRT"
-$Env:PATH = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;" + $Env:PATH
 $Env:TRT_LIBPATH="D:\lib\TensorRT-8.2.3.0"
 
-$Env:PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\bin" + $Env:PATH
+$Env:PATH = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;" + $Env:PATH
+# MSBuild version shall be 15.9.21.664
+#msbuild /version
 
-
- 
 pushd $Env:TRT_OSSPATH
-mkdir -p build ; cd build
-cmake .. -G "Visual Studio 15 2017"  -DTRT_LIB_DIR=$Env:TRT_LIBPATH -DTRT_OUT_DIR='..\out' -DCMAKE_TOOLCHAIN_FILE="..\cmake\toolchains\cmake_x64_win.toolchain" -DCUDA_VERISON="11.4.3" -DCUDA_TOOLKIT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.4" -DCMAKE_CUDA_ARCHITECTURES=all -DCMAKE_SYSTEM_PROCESSOR="AMD64"
- 
+
+# Clean build, useful for debugging
+if (Test-Path build) {
+    rmdir -r build 
+}
+if (!(Test-Path build)) {
+    mkdir -p build
+}
+
+cd build
+
+# We use VS 2017 as suggested in the README.md. cl.exe version shall be 19.16.27045.
+
+cmake .. -G "Visual Studio 15 2017" -DTRT_LIB_DIR=$Env:TRT_LIBPATH -DTRT_OUT_DIR='$(Get-Location)\out' -DCMAKE_TOOLCHAIN_FILE=..\cmake\toolchains\cmake_x64_win.toolchain 
+
+#-DCMAKE_CUDA_ARCHITECTURES="75" 
  
 # msbuild ALL_BUILD.vcxproj
  
 popd
+
+
+
+
+ 
+# cmake .. -G "Visual Studio 15 2017"  -DTRT_LIB_DIR=$Env:TRT_LIBPATH -DTRT_OUT_DIR='..\out' -DCMAKE_TOOLCHAIN_FILE="..\cmake\toolchains\cmake_x64_win.toolchain" -DCUDA_VERISON="11.4.3" -DCUDA_TOOLKIT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.4" -DCMAKE_CUDA_ARCHITECTURES=all -DCMAKE_SYSTEM_PROCESSOR="AMD64"
+ 
  
